cd2500270a530e82e82c18078e14a36ecef578ef
viniciusofp <viniciusofp@gmail.com>
2016-11-30 23:47:39 -0200

    php to html

--- header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>d3.js Experiment</title>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.min.css" rel="stylesheet">
-
-</head>
-<body>
